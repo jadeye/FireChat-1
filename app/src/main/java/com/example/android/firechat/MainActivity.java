83fcbe4699d7dcf510f6a2eaa517870366b114e9
WRFitch <WRFitch@outlook.com>
2019-01-09 23:27:24 +0000

    Updated firebase dependencies

--- app/src/main/java/com/example/android/firechat/MainActivity.java
+++ app/src/main/java/com/example/android/firechat/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.android.firechat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firebase stuff
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
 
-    
+
 }
