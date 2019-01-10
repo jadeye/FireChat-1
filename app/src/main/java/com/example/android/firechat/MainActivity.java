7b5626819a0a18e80b087defbbf3c1c31eae472b
WRFitch <WRFitch@outlook.com>
2019-01-10 17:28:28 +0000

    Finished user login

--- app/src/main/java/com/example/android/firechat/MainActivity.java
+++ app/src/main/java/com/example/android/firechat/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.android.firechat;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -20,29 +27,35 @@ public class MainActivity extends AppCompatActivity {
 
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
+    String mVerificationId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
 
+        userIsLoggedIn();
 
         mPhoneNumber = findViewById(R.id.phoneNumber);
         verificationCode = findViewById(R.id.verificationCode);
-
         sendVerificationButton = findViewById(R.id.sendVerificationButton);
 
         sendVerificationButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                startPhoneNumberVerification();
+                if(mVerificationId != null){
+                    verifyPhoneNumberWithCode();
+                } else {
+                    startPhoneNumberVerification();
+                }
             }
         });
 
         mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-
+                signInWithPhoneAuthCredential(phoneAuthCredential);
             }
 
             @Override
@@ -50,7 +63,45 @@ public class MainActivity extends AppCompatActivity {
                 //no failure condition in tutorial - bad practise?
                 //at least throw up a snackbar
             }
+
+            @Override
+            public void onCodeSent(String mVerification,
+                                   PhoneAuthProvider.ForceResendingToken
+                                           forceResendingToken){
+                super.onCodeSent(mVerification, forceResendingToken);
+
+                mVerificationId = mVerification;
+               sendVerificationButton.setText("Verify Code");
+            }
         };
+    }
+
+    private void verifyPhoneNumberWithCode(){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(
+                mVerificationId,
+                verificationCode.getText().toString());
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            userIsLoggedIn();
+                        }
+                    }
+                });
+    }
+
+    private void userIsLoggedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            startActivity(new Intent(getApplicationContext(), MainPageActivity.class));
+            finish();
+            return;
+        }
     }
 
     private void startPhoneNumberVerification() {
