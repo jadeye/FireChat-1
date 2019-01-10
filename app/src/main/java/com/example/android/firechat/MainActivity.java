b87223643a0ab9ed43b58852df23777014bcb54b
WRFitch <WRFitch@outlook.com>
2019-01-10 00:04:22 +0000

    added basic authentication screen

--- app/src/main/java/com/example/android/firechat/MainActivity.java
+++ app/src/main/java/com/example/android/firechat/MainActivity.java
@@ -2,23 +2,65 @@ package com.example.android.firechat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    private EditText mPhoneNumber, verificationCode;
+    private Button sendVerificationButton;
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
 
-        //firebase stuff
-        mAuth = FirebaseAuth.getInstance();
 
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        verificationCode = findViewById(R.id.verificationCode);
+
+        sendVerificationButton = findViewById(R.id.sendVerificationButton);
+
+        sendVerificationButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startPhoneNumberVerification();
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                //no failure condition in tutorial - bad practise?
+                //at least throw up a snackbar
+            }
+        };
     }
 
+    private void startPhoneNumberVerification() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                mPhoneNumber.getText().toString(),
+                60,
+                TimeUnit.SECONDS,
+                this,
+                mCallbacks);
+    }
 
 
 }
